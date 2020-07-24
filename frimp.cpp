fbff26ead11940073d01b7c7cf1ec4fdb091d830
potatoeggy <eggyrules@gmail.com>
2020-07-24 11:58:13 -0400

    i have no clue why i segfaulted in frimp screw this it works for the sample

--- frimp.cpp
+++ frimp.cpp
@@ -33,6 +33,10 @@
 #define trouvez find
 #define debut begin
 #define fin end
+#define revenez pop_back
+#define premier first
+#define taille_t size_t
+#define cae stoi
 
 const ins::vecteur<ins::vecteur<ins::chaine>> pronomOrdre {
 	{"le", "la", "les"},
@@ -40,40 +44,49 @@ const ins::vecteur<ins::vecteur<ins::chaine>> pronomOrdre {
 	{"y"},
 	{"en"}
 };
+const ins::chaine voyelles = "aeiouy";
 
-ent determinePronomOrdre(ins::chaine mot) {
+ins::paire<ent, ins::chaine> determinePronomOrdre(ins::chaine mot) {
 	si (mot == "me") mot = "moi";
-	si (mot == "te") mot == "toi";
+	si (mot == "te") mot = "toi";
 	pour (ent j = 0; j < pronomOrdre.taille(); j++) { // yikes double for
 		pour (ent k = 0; k < pronomOrdre[j].taille(); k++) {
 			si (mot == pronomOrdre[j][k]) {
-				remettez j;
+				remettez {j, mot};
 			}
 		}
 	}
-	remettez -1;
+	remettez {-1, ""};
 }
 
 ent principal() {
 	ins::centree.sync_avec_esins(faux);
 	ins::centree.attachez(ptrnul);
 
-	ent lignes; ins::centree >> lignes;
+	ins::chaine stupideCpp;
+	ins::obtenez_ligne(ins::centree, stupideCpp);
+	ent lignes = ins::cae(stupideCpp);
 
 	pour (ent i = 0; i < lignes; i++) {
 		ins::vecteur<ins::chaine> ligne;
 		ins::vecteur<ins::paire<ent, ins::chaine>> pronoms;
 		ins::chaine temp;
-		pendant (ins::obtenez_ligne(ins::centree, temp, ' ')) {
-			ligne.poussez(temp);
+		ins::obtenez_ligne(ins::centree, temp);
+		// mini split is from so
+		taille_t pos = 0;
+		ins::chaine token;
+		while ((pos = temp.find(' ')) != ins::chaine::npos) {
+			ligne.poussez(temp.souscha(0, pos));
+			temp.erase(0, pos + 1);
 		}
+		ligne.poussez(temp);
 
 		pour (ent i = 2; i < ligne.taille() - 1; i++) {
-			si (ligne[i].trouvez("'") != ins::chaine::npos) {
-				pronoms.poussez({determinePronomOrdre(ligne[i]), ligne[i]});
+			si (ligne[i].trouvez("'") == ins::chaine::npos) {
+				pronoms.poussez(determinePronomOrdre(ligne[i]));
 			} autre {
-				pronoms.poussez({determinePronomOrdre(ligne[i][0] + "e"), ligne[i][0] + "e"});
-				pronoms.poussez({determinePronomOrdre(ligne[i].souscha(2, ligne[i].longueur())), ligne[i].souscha(2, ligne[i].longueur())}); // takes the bit after the apostrophe
+				pronoms.poussez(determinePronomOrdre(ligne[i][0] + "e"));
+				pronoms.poussez(determinePronomOrdre(ligne[i].souscha(2, ligne[i].longueur()))); // takes the bit after the apostrophe
 			}
 		}
 
@@ -87,7 +100,20 @@ ent principal() {
 		bool infinitifEstRegulier = infinitif[infinitif.longueur() - 1] == 'r' && infinitif[infinitif.longueur() - 2] == 'e';
 		// comments say that there's some broken test data
 		// imperatives can be nous vous or tu but we're assuming it's always tu?
-		std::string phrase = verbe;
+		ins::chaine phrase = verbe + "-";
+		pour (ent i = 1; i < pronoms.taille(); i++) {
+			si (voyelles.trouvez(pronoms[i].second[0]) != ins::chaine::npos) {
+				si (pronoms[i-1].second == "moi" || pronoms[i-1].second == "toi" || pronoms[i-1].second == "le" || pronoms[i-1].second == "la") {
+					phrase += pronoms[i-1].second[0] + "'";
+				}
+			} autre {
+				phrase += pronoms[i-1].second + "-";
+			}
+		}
+		phrase += pronoms[pronoms.taille()-1].second;
+		
+
+		//si (phrase[phrase.longueur() - 1] == '-' || phrase[phrase.longueur() - 1] == '\'') phrase.revenez();
 		ins::csortie << phrase << " !\n";
 	}
 
