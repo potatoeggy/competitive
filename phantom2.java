96b164e3016080752bff5e708f319bf68b1b6bf0
potatoeggy <eggyrules@gmail.com>
2020-10-18 12:23:28 -0400

    fix phantom2 in java

--- phantom2.java
+++ phantom2.java
@@ -21,7 +21,7 @@ public class phantom2 {
 	public static void main(String[] args) throws IOException {
 		boolean[] primes = new boolean[1000001];
 		primes[1] = true;
-		for (int i = 3; i * i < primes.length; i += 2) {
+		for (int i = 2; i * i < primes.length; i++) {
 			if (! primes[i]) {
 				for (int j = i * i; j <= 1000000; j += i) {
 					primes[j] = true;
@@ -29,7 +29,7 @@ public class phantom2 {
 			}
 		}
 		int cases = nextInt();
-		for (int i = 0; i < cases; i++) { // i don't understand the question
+		for (int i = 0; i < cases; i++) {
 			int c = 0;
 			int base = nextInt();
 			int ceil = nextInt();
